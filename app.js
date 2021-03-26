69f5a4f5a8b1ef49649f1ca80f444d0a3e0d75cc
Nicolas Martins <nicolas.martins@hotmail.fr>
2021-03-26 15:50:17 +0100

    added a GET route and some data to link the backend with the frontend. + added handler for CORS. Working fine

--- app.js
+++ app.js
@@ -2,8 +2,56 @@ const express = require('express')
 
 const app = express()
 
-app.use((req, res) => {
-  res.json({ message: 'Votre requête a bien été reçue !' })
+//Handle CORS
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization'
+  )
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PUT, DELETE, PATCH, OPTIONS'
+  )
+  next()
+})
+
+app.use('/api/products', (req, res, next) => {
+  const productData = [
+    {
+      id: '2153248944598',
+      name: 'Xiaomi Redmi Note 8T',
+      price: 500,
+      category: 'Portable Android',
+      description: "Un PC vendu comme neuf, n'a quasiment jamais été utilisé.",
+      image: 'https://picsum.photos/250/200',
+    },
+    {
+      id: '54542318415',
+      name: 'Ordinateur portable HP',
+      price: 1500,
+      category: 'Ordinateur Portable',
+      description: "Un PC vendu comme neuf, n'a quasiment jamais été utilisé.",
+      image: 'https://picsum.photos/250/200',
+    },
+    {
+      id: '5498454542318415',
+      name: 'MacBook Pro 17 pouces',
+      price: 2000,
+      category: 'Ordinateur MacOS',
+      description: "Un PC vendu comme neuf, n'a quasiment jamais été utilisé.",
+      image: 'https://picsum.photos/250/200',
+    },
+    {
+      id: '778742318415',
+      name: 'iPhone X Max',
+      price: 400,
+      category: 'Portable Apple',
+      description: "Un PC vendu comme neuf, n'a quasiment jamais été utilisé.",
+      image: 'https://picsum.photos/250/200',
+    },
+  ]
+  res.status(200).json(productData)
 })
 
 module.exports = app
